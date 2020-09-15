73fb687be1710669c3d5089825c16cd38a884084
cylee1222 <55579898+cylee1222@users.noreply.github.com>
2020-09-15 15:24:24 +0800

    Add files via upload

---
+++ merge_sort.h
@@ -0,0 +1,7 @@
+#ifndef MERGE_SORT_H_
+#define MERGE_SORT_H_
+
+void merge_4_way(int* array, int low, int mid1, int mid2, int mid3, int high);
+void mergesort_4_way_rec(int* array, int low, int high);
+
+#endif // MERGE_SORT_H_
