ed64aff17a9186ba8d6008ae136582983faa669a
Lee Chun Yin <cylee@workbench.cs.hku.hk>
2020-09-25 12:09:52 +0800

    123

--- merge_sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef MERGE_SORT_H_
-#define MERGE_SORT_H_
-
-void merge_4_way(int* array, int low, int mid1, int mid2, int mid3, int high);
-void mergesort_4_way_rec(int* array, int low, int high);
-
-#endif // MERGE_SORT_H_
